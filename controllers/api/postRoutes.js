a8990da8de6293761241327525162e4864bf515d
gibsona475 <81275092+gibsona475@users.noreply.github.com>
2021-09-06 16:07:11 -0700

    Added post and signup js and handlebars

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,14 +1,27 @@
 const { Post } = require("../../models");
-
+const router = require('express').Router();
 
 //VIEW ALL POSTS 
-router.get("/users", (req,res) =>{
+router.get("/posts", (req,res) =>{
     const postData = Post.findAll({}); 
     //render your neww handlebar with data fro mthe database 
     res.render("/dashboard", {posts: postData})
 }); 
 //VIEW POST BY ID 
-
+router.get("/posts/:id", (req,res) =>{
+    const postData = Post.findAll({}); 
+    //render your neww handlebar with data fro mthe database 
+    res.render("/dashboard", {posts: postData})
+}); 
+router.post("/", (req,res) =>{
+    console.log("Creating a new post", req.body);
+    const postData = Post.create(req.body); 
+    // res.status(200).json(postData);
+    //render your neww handlebar with data fro mthe database 
+    res.render("/dashboard", {posts: postData})
+});
 //UPDATE POST BY ID 
 
 //DELETE POST BY ID 
+
+module.exports  = router; 
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -24,4 +24,34 @@ router.get("/signup", (req,res) =>{
     res.render('signup'); 
 }); 
 
+router.get('/dashboard/new', (req,res)=>{
+  res.render('newpost');
+}); 
+
+router.get('/', async (req, res) => {
+  try {
+    // Get all posts and JOIN with user data
+    const postData = await Post.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['username'],
+        },
+      ],
+    });
+
+    // Serialize data so the template can read it
+    const posts = postData.map((post) => post.get({ plain: true }));
+    console.log("View all posts", posts);
+    // Pass serialized data and session flag into template
+    res.render('dashboard', { 
+      posts, 
+      logged_in: req.session.logged_in 
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
 module.exports = router; 
